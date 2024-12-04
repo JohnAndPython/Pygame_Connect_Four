d72f335d4741add4708f73e0fa42d95d0f641e78
JohnJames <barthos@gmx.de>
2024-12-04 11:23:05 +0100

    update player.py

--- player.py
+++ player.py
@@ -1,9 +1,26 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, color):
+    def __init__(self, color: str, center_init_pos) -> None:
         super().__init__()
 
-        self._color = color
+        self._color: str = color
+        self.image: pygame.surface.Surface = None
 
-        self._turn = False
+        self._turn: bool = False
+
+
+        if color == "red":
+            self.image: pygame.surface.Surface = pygame.image.load(r"Assets\coin_red.png")
+        elif color == "yellow":
+            self.image: pygame.surface.Surface = pygame.image.load(r"Assets\coin_yellow.png")
+
+        self.rect = self.image.get_rect()
+        self.rect.centery = center_init_pos
+
+    def set_centerx_to(self, pos_x: int) -> None:
+        self.rect.centerx = pos_x
+
+
+    def draw(self, surface: pygame.surface.Surface) -> None:
+        surface.blit(self.image, self.rect)
