06d327727bb98b0950b27551862415034e51b341
JohnJames <barthos@gmx.de>
2024-12-05 09:01:55 +0100

    Set variables to private

--- board.py
+++ board.py
@@ -5,38 +5,41 @@ class Board(pygame.sprite.Sprite):
     def __init__(self, board_size_y: int=7, board_size_x: int=6) -> None:
         super().__init__()
 
-        self.board: list = []
-        self.board_size_y: int = board_size_y
-        self.board_size_x : int= board_size_x
+        self._board: list = []
+        self._board_size_y: int = board_size_y
+        self._board_size_x: int= board_size_x
 
-        for _ in range(self.board_size_x):
-            self.board.append(self.board_size_y * [0])
+        for _ in range(self._board_size_x):
+            self._board.append(self._board_size_y * [0])
 
-        self.board_rects: list = []
+        self._board_rects: list = []
 
-        self.image: pygame.surface.Surface = pygame.image.load(r"Assets\board.png").convert_alpha()
-        self.rect: pygame.rect.Rect = self.image.get_rect()
+        self._image: pygame.surface.Surface = pygame.image.load(r"Assets\board.png").convert_alpha()
+        self._rect: pygame.rect.Rect = self._image.get_rect()
 
         # Fill nested 2D List with Pygame rectangles
         left_pos: int = 0
         top_pos: int = 120
         temp_lst: list = []
 
-        for _ in range(self.board_size_x):
-            for _ in range(self.board_size_y):
-                temp_lst.append(self.image.get_rect(left=left_pos, top=top_pos))
+        for _ in range(self._board_size_x):
+            for _ in range(self._board_size_y):
+                temp_lst.append(self._image.get_rect(left=left_pos, top=top_pos))
                 left_pos += 100
 
-            self.board_rects.append(temp_lst)
+            self._board_rects.append(temp_lst)
             temp_lst = []
             left_pos = 0
             top_pos += 100
 
 
+    
+
+
     def get_centersx(self) -> list[int]:
         ''' Return a list with the centerx value of each rect in the first row of self.board_rects'''
 
-        return [rect.centerx for rect in self.board_rects[0]]
+        return [rect.centerx for rect in self._board_rects[0]]
     
 
     def get_points_pos(self, offset_width: int=10, offset_height: int=8) -> list[tuple]:
@@ -45,7 +48,7 @@ class Board(pygame.sprite.Sprite):
         points = []
         pointx_1, pointy_1, pointx_2, pointy_2 = 0, 0, 0, 0
          
-        for rect in self.board_rects[0]:
+        for rect in self._board_rects[0]:
             pointx_1 = rect.topleft[0] + offset_width
             pointy_1 = rect.topleft[1] - offset_height
             pointx_2 = rect.topright[0] - offset_width
@@ -59,7 +62,7 @@ class Board(pygame.sprite.Sprite):
     def col_full(self, index) -> bool:
         ''' Check if a column is full or not. Full = Column is completly filled with values other than zeros'''
 
-        for value in self.board:
+        for value in self._board:
             if value[index] != 0:
                 return True
             else:
@@ -74,6 +77,6 @@ class Board(pygame.sprite.Sprite):
 
     def draw(self, surface: pygame.surface.Surface) -> None:
         ''' Draw the rectangles of the nested list self.board_rects on surface'''
-        for row in self.board_rects:
+        for row in self._board_rects:
             for rect in row:
-                surface.blit(self.image, rect)
+                surface.blit(self._image, rect)
