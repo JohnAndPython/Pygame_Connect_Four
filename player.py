958e929ef5464185b33d43b6f08d3d421feb61c4
JohnJames <barthos@gmx.de>
2024-12-05 10:16:48 +0100

    added 3 functions

--- player.py
+++ player.py
@@ -8,12 +8,14 @@ class Player(pygame.sprite.Sprite):
         self._image: pygame.surface.Surface = None
 
         self._turn: bool = False
-
+        self.value: int = 0
 
         if color == "red":
             self._image: pygame.surface.Surface = pygame.image.load(r"Assets\coin_red.png").convert_alpha()
+            self.value = 1
         elif color == "yellow":
             self._image: pygame.surface.Surface = pygame.image.load(r"Assets\coin_yellow.png").convert_alpha()
+            self.value = -1
 
         self._rect: pygame.rect.Rect = self._image.get_rect()
         self._rect.centery: int = center_init_pos
@@ -28,6 +30,17 @@ class Player(pygame.sprite.Sprite):
     def animation_fall(self, centerx) -> None:
         self._rect.centerx = centerx
         self._rect.centery += 5
+
+    def set_coin_bottom(self, value: int) -> int:
+        self._rect.bottom = value
+
+
+    def get_coin_bottom(self) -> int:
+        return self._rect.bottom
+
+
+    def get_coin_height(self) -> int:
+        return self._rect.height
 
 
     def draw(self, surface: pygame.surface.Surface) -> None:
